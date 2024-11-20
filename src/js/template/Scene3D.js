c31aef865a297b4be59f9fc91970b49389ea77a8
Adrien Husson <husson.adr@gmail.com>
2024-11-20 12:33:24 +0100

    init scenario gravity cubes

--- src/js/template/Scene3D.js
+++ src/js/template/Scene3D.js
@@ -2,8 +2,10 @@ import * as THREE from 'three'
 import GlobalContext from "./GlobalContext"
 import DomElement from "../Utils/DomElement"
 
-export default class Scene3D {
+export default class Scene3D extends THREE.Scene {
     constructor(id) {
+        super()
+
         /** context (window) */
         this.globalContext = new GlobalContext()
         this.globalContext.addScene(this)
@@ -21,18 +23,11 @@ export default class Scene3D {
         // this.context = this.canvas.getContext("3d")
 
         /** example */
-        this.scene = new THREE.Scene()
         this.camera = new THREE.PerspectiveCamera( 75, this.domElement.aspectRatio, 0.1, 1000 )
         this.renderer = new THREE.WebGLRenderer({
             canvas: this.canvas, antialias: true
         })
-
-        const geometry = new THREE.BoxGeometry( 1, 1, 1 )
-        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
-        this.cube = new THREE.Mesh( geometry, material )
-        this.scene.add( this.cube )
-        this.camera.position.z = 5
-
+        
         /** init */
         this.resize()
     }
@@ -43,12 +38,12 @@ export default class Scene3D {
 
     resize() {
         this.domElement.setSize()
-        this.canvas.width = this.domElement.width
-        this.canvas.height = this.domElement.height
 
+        /** resize camera */
         this.camera.aspect = this.domElement.aspectRatio
         this.camera.updateProjectionMatrix()
 
+        /** resize renderer */
         this.renderer.setSize( this.width, this.height, false )
         this.renderer.setPixelRatio(this.globalContext.window.pixelRatio)
     }
@@ -56,8 +51,7 @@ export default class Scene3D {
     onDeviceOrientation() {}
 
     update() { 
-        this.cube.rotation.z += this.globalContext.time.delta / 1000
-        this.renderer.render( this.scene, this.camera )
+        this.renderer.render( this, this.camera )
     }
 
     scroll() {
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import SceneBouncingBubbles from "./js/scenarios/SceneBouncingBubbles"
-import Scene3D from "./js/template/Scene3D"
+import SceneGravityCubes from "./js/scenarios/SceneGravityCubes"
 import { askMotionAccess } from "./js/Utils/DeviceAccess"
 
 const scene2d = new SceneBouncingBubbles("canvas-scene")
-const scene3d = new Scene3D("canvas-scene-3d")
+const scene3d = new SceneGravityCubes("canvas-scene-3d")
 const scene2d2 = new SceneBouncingBubbles("canvas-scene2")
 
 /** motion sensors authorization */
